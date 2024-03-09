ac66c9ac8116d2cf2997c353e5315ce3c3c06b4b
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-03-09 20:52:55 +0100

    startnig project and creating first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import banner1 from "./assets/img/banner-1.jpeg";
+// import banner2 from "./assets/img/banner-2.jpg";
+// import banner3 from "./assets/img/banner-3.jpg";
+
+const product = { title: "Samsung", price: 500, stock: 10 };
+const product1 = { title: "Iphone", price: 600, stock: 2 };
+const product2 = { title: "Xiaomi", price: 400, stock: 7 };
 
 function App() {
+  // komponens létrehozása
+  function Card(props) {
+    const { name, price, stock } = props;
+    console.log(name, price, stock);
+
+    return (
+      <div className="card">
+        <div className="card-title">
+          <h2>{name}</h2>
+        </div>
+        <div className="card-body">
+          <p>{price} $</p>
+          <p>{stock} darab van készleten</p>
+          <button>Kosárhoz</button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Webshop</h1>
+      <Card name={product.title} price={product.price} stock={product.stock} />
+      <Card
+        name={product1.title}
+        price={product1.price}
+        stock={product1.stock}
+      />
+      <Card
+        name={product2.title}
+        price={product2.price}
+        stock={product2.stock}
+      />
+
+      {/* <div className="banner">
+        <img src={banner1} alt="banner" />
+        <img src={banner2} alt="banner" />
+        <img src={banner3} alt="banner" />
+      </div> */}
     </div>
   );
 }
